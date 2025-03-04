287682a34d8df90f1fe592b09a875fcbb2951adc
jlanej <jlanej@gmail.com>
2025-03-04 14:46:36 -0600

    test build jar

--- src/main/Run.java
+++ src/main/Run.java
@@ -13,36 +13,41 @@ import toolkit.SummaryStatisticsForBatchFile;
  * Programmer / Software Development Engineer
  * Institute for Informatics
  * Washington University School of Medicine in St. Louis
- * 
- * Date: August 30, 2021 
+ * <p>
+ * Date: August 30, 2021
  */
 public class Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		try {
-			System.out.println(LocalDateTime.now());
-	        System.out.println("Start");
-	        // The "[path_to_the_karyotype_input_file]" is where your karyotypes input is saved, e.g. "C:\\Karyotype_samples.txt"
-	        // The "[path_to_the_output_JSON_file]" is where you want to save the JSON output, e.g. "C:\\Karyotype_samples.json" 
-		    // The "[path_to_the_output_aggregate_LGF_csv_file]" is where you want to save the aggregate LGF output, e.g. "C:\\Karyotype_samples.csv" 
-		    // The "[path_to_the_output_summary_statistics_csv_file]" is where you want to save the summary statistics output, e.g. "C:\\Karyotype_samples_SummaryStatistics.csv" 
-			AggregateJsonForBatchFile.generateJson(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_JSON_file]"));
-			AggregateCsvForBatchFile.generateAggregateLGF(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_aggregate_LGF_csv_file]"));
-	        SummaryStatisticsForBatchFile.writeSummaryStatistics(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_summary_statistics_csv_file]"));
-	        System.out.println(LocalDateTime.now());
-    		System.out.println("Complete");
-		} catch (Exception e) {
-			e.printStackTrace(); 
-			System.out.println(LocalDateTime.now());
-    		System.out.println("Incomplete");
-		}
-		
-			
-	}
-	
-	
-	
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(LocalDateTime.now());
+            System.out.println("Start");
+            // The "[path_to_the_karyotype_input_file]" is where your karyotypes input is saved, e.g. "C:\\Karyotype_samples.txt"
+            // The "[path_to_the_output_JSON_file]" is where you want to save the JSON output, e.g. "C:\\Karyotype_samples.json"
+            // The "[path_to_the_output_aggregate_LGF_csv_file]" is where you want to save the aggregate LGF output, e.g. "C:\\Karyotype_samples.csv"
+            // The "[path_to_the_output_summary_statistics_csv_file]" is where you want to save the summary statistics output, e.g. "C:\\Karyotype_samples_SummaryStatistics.csv"
+//			AggregateJsonForBatchFile.generateJson(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_JSON_file]"));
+//			AggregateCsvForBatchFile.generateAggregateLGF(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_aggregate_LGF_csv_file]"));
+//	        SummaryStatisticsForBatchFile.writeSummaryStatistics(Paths.get("[path_to_the_karyotype_input_file]"), Paths.get("[path_to_the_output_summary_statistics_csv_file]"));
+//           	print args
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+            AggregateJsonForBatchFile.generateJson(Paths.get(args[0]), Paths.get(args[1]));
+            AggregateCsvForBatchFile.generateAggregateLGF(Paths.get(args[0]), Paths.get(args[2]));
+            SummaryStatisticsForBatchFile.writeSummaryStatistics(Paths.get(args[0]), Paths.get(args[3]));
+            System.out.println(LocalDateTime.now());
+
+            System.out.println("Complete");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(LocalDateTime.now());
+            System.out.println("Incomplete");
+        }
+
+
+    }
+
 
 }
